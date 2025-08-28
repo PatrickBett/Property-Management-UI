313e4bdf306101cd5f896ef39716b87fb31f613f
Patrick <patrickbett018@gmail.com>
2025-08-28 10:44:43 +0300

    categorycontext

--- src/components/Contexts/CategoryContext.jsx
+++ src/components/Contexts/CategoryContext.jsx
@@ -3,35 +3,28 @@ import api from "../../api";
 // create a context
 export const CategoryContext = createContext();
 
+export const CategoryProvider = ({ children }) => {
+  const [categories, setCategories] = useState([]);
+  const token = localStorage.getItem("access");
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await api.get("api/categories/", {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        console.log("Categories of houses", res.data);
+        setCategories(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-export const CategoryProvider =({children})=>{
-    const [categories, setCategories] = useState([])
-    const token = localStorage.getItem("access");
-
-    useEffect(()=>{
-
-        const fetchCategories = async()=>{
-            try{
-                const res =  await api.get("http://127.0.0.1:8000//api/categories/", {
-                    headers: { Authorization: `Bearer ${token}` },
-                  })
-            console.log(res.data)
-            setCategories(res.data)
-            }catch(error){
-                console.log(error)
-            }
-        
-        }
-    
-    
-        fetchCategories()
-    },[token])
-    return(
-        <CategoryContext.Provider value={{categories, setCategories}}>
-            {children}
-        </CategoryContext.Provider>
-    );
-    
+    fetchCategories();
+  }, [token]);
+  return (
+    <CategoryContext.Provider value={{ categories, setCategories }}>
+      {children}
+    </CategoryContext.Provider>
+  );
 };
-
